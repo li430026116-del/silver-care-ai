833db57ab2d357fceeea06eccce2392094cfca31
JellyFish.zip <li430026116@gmail.com>
2026-06-16 13:05:18 +0800

    Update settings.py

--- NuanYa/nuanya/settings.py
+++ NuanYa/nuanya/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-bu72m85_oee&s3lzk7rd)e619jnsg^ajej%zr%k8p&dn@cb0an"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "dev-placeholder-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -91,7 +91,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "nuanya",  # 数据库名
         "USER": "root",  # 用户名
-        "PASSWORD": "Zoey0733",  # 密码
+        "PASSWORD": os.environ.get("MYSQL_PASSWORD", ""),,  # 密码
         "HOST": "127.0.0.1",
         "PORT": "3306",
         "OPTIONS": {
